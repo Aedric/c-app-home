b531100b6f994318d60fffae14e7643b3780a6de
Aedric Donovan <aedric@Aedrics-MacBook-Air.local>
2019-09-28 16:15:04 -0500

    remove npm

--- client/applets/window.js
+++ client/applets/window.js
@@ -2,10 +2,9 @@ export const windowLoad = (applets) => {
     
     for ( var i=0, len = applets.length; i < len; i++ ) {
       var applet = applets[i];
-      var draggie = new Draggabilly( applet, {
+      var draggie = new Draggabilly(applet, {
         handle: '.handle'
-      }
-      );
+      });
       console.log(draggie);
     }
   
