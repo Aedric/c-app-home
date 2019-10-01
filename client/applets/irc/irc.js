b5ec628eff324e3232984fbb7bece1583aadae0c
Aedric Donovan <aedric@Aedrics-MacBook-Air.local>
2019-09-30 19:09:28 -0500

    spacing and formatting. 4 spaces => 2 spaces

--- client/applets/irc/irc.js
+++ client/applets/irc/irc.js
@@ -1,7 +1,7 @@
 import { load } from '../../utils/api';
 import { mockload } from '../../utils/mock';
 
-const getUrl = `${ env.IRC_SERVER_URL }${ env.IRC_GET }`;
+const getUrl = `${env.IRC_SERVER_URL}${env.IRC_GET}`;
 const pageSize = env.IRC_PAGE_SIZE < 4 ? env.IRC_PAGE_SIZE : 4;
 
 let activeChat = {};
@@ -11,10 +11,10 @@ let chatLog = [];
 load(getUrl, handleDataLoad);
 
 handleDataLoad = (data) => {
-    // handle that data
-    parseChatPayload(0, data.chatPayload).then((parsedChat) => {
-        console.log(parsedChat);
-    }) 
+  // handle that data
+  parseChatPayload(0, data.chatPayload).then((parsedChat) => {
+    console.log(parsedChat);
+  })
 }
 
-parseChatPayload = (data) => { return data.replace(/\r\n|\n\r|\n|\r/g,"\n").split("\n"); }
+parseChatPayload = (data) => { return data.replace(/\r\n|\n\r|\n|\r/g, "\n").split("\n"); }
--- client/applets/window.js
+++ client/applets/window.js
@@ -1,11 +1,10 @@
 export const windowLoad = (applets) => {
-    
-    for ( var i=0, len = applets.length; i < len; i++ ) {
-      var applet = applets[i];
-      var draggie = new Draggabilly(applet, {
-        handle: '.handle'
-      });
-      console.log(draggie);
-    }
-  
+  for ( var i=0, len = applets.length; i < len; i++ ) {
+    var applet = applets[i];
+    var draggie = new Draggabilly(applet, {
+      handle: '.handle'
+    });
+    console.log(draggie);
   }
+
+}
